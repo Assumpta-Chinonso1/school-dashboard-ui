994af5eb2de4239dc38530e46f7da33b5a26cefe
Assumpta <assumptachinonso42@gmail.com>
2026-05-04 05:53:57 +0100

    styled the search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='flex items-center justify-between p-4'>
 
       {/*SEARCH BAR*/}
-      <div className="hidden md:flex">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 p-2">
         <Image src="/search.png" alt="" width={14} height={14} />
         <input type="text" placeholder="Search..." />
       </div>
@@ -14,13 +14,15 @@ const Navbar = () => {
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/message.png" alt="" width={20} height={20} />
         </div>
-        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
           <Image src="/announcement.png" alt="" width={20} height={20} />
+          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">1</div>
         </div>
         <div className="flex flex-col">
-          <span className="text-sm leading-3 font-medium">AssumDevs</span>
-          <span className="">Admin</span>
+          <span className="text-xl leading-3 font-medium">AssumDevs</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
+        <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full" />
       </div>
 
     </div>
