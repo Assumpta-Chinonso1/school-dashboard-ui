a90955f2c14d14f80d9d99f7b95ccbfd8fc55641
Assumpta <assumptachinonso42@gmail.com>
2026-05-05 04:59:23 +0100

    worked on the admin

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,11 @@
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/*LEFT*/}
+      <div className=""></div>
+      {/*RIGHT*/}
+      <div className=""></div>
+    </div>
   );
 };
 
