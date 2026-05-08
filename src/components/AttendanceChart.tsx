673b4e53891074ed7d16233c5365cf2b8cbbc0de
Assumpta <assumptachinonso42@gmail.com>
2026-05-08 05:42:17 +0100

    new commit

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -50,12 +50,8 @@ const AttendanceChart = () => {
         <BarChart
             data={data}
             width={500}
-            
-            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-
-        >
-
-
+            height={300}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }} >
 
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
@@ -64,6 +60,7 @@ const AttendanceChart = () => {
       <Legend />
       <Bar dataKey="pv" fill="#8884d8" activeBar={{ fill: 'pink', stroke: 'blue' }} radius={[10, 10, 0, 0]} />
       <Bar dataKey="uv" fill="#82ca9d" activeBar={{ fill: 'gold', stroke: 'purple' }} radius={[10, 10, 0, 0]} />
+      </BarChart>
       
     </ResponsiveContainer>
     </div>
