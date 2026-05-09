11cb0b6513b867320c3fbaa82b949bea5edf5f47
Assumpta <assumptachinonso42@gmail.com>
2026-05-09 05:25:11 +0100

    new coomit

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -55,7 +55,7 @@ const AttendanceChart = () => {
       <CartesianGrid strokeDasharray="3 3" vertical={false} stroke='#ddd' />
       <XAxis dataKey="name" axisLine={false} tick={{fill: "#d1d5db"}} tickLine={false} />
       <YAxis axisLine={false} tick={{fill: "#d1d5db"}} tickLine={false}/>
-      <Tooltip />
+      <Tooltip contentStyle={{borderRadius: "10px", borderColor: "lightgray"}} />
       <Legend  align="left" verticalAlign="top" wrapperStyle={{paddingTop:"20px", paddingBottom:"40px" }} />
       <Bar dataKey="present" fill="#FAE27C" legendType='circle' radius={[10, 10, 0, 0]}/>
       
