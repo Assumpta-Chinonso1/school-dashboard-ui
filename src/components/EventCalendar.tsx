081e3c92f0dcf354e05e7d223c89548a1c2f74b0
Assumpta <assumptachinonso42@gmail.com>
2026-05-11 05:50:25 +0100

    new commit

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -23,3 +23,31 @@ export default EventCalendar;*/}
 
 
 
+"use client"
+
+//import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Meeting",
+    start: new Date(2026, 4, 11, 10, 0),
+    end: new Date(2026, 4, 11, 12, 0),
+  },
+];
+
+export default function MyCalendar() {
+  return (
+    <div className="h-[500px]">
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
+    </div>
+  );
+}
