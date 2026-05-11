fb5f1dcd9a58d70eb9c9712dcb0ad725f7a423c6
Assumpta <assumptachinonso42@gmail.com>
2026-05-11 05:56:47 +0100

    new commit

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -39,7 +39,9 @@ const AdminPage = () => {
       
       </div>
       {/*RIGHT*/}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+      
+      </div>
     </div>
   );
 };
