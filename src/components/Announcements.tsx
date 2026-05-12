e8422a03d7705aeb03f68e33aea7c446b8afc382
Assumpta <assumptachinonso42@gmail.com>
2026-05-12 06:07:44 +0100

    new commit

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,44 @@
+const Announcements = () => {
+  return (
+    <div className='bg-white p-4 rounded-md'>
+     <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Announcements</h1>
+        <span className="text-xs font-gray-400">View all</span>
+     </div>
+     <div className="flex flex-col gap-4 mt-4">
+
+
+     <div className="bg-assumDevsLight rounded-md p-4">
+        <div className="flex items-center justify-between">
+            <h2 className="font-medium">New student enrollment</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-2 py-2">2026-12-5</span>
+        </div>
+        <p className="text-sm text-gray-500 mt-2">
+          We are excited to announce that enrollment for the upcoming semester is now open. Please visit the admissions office for more information and to submit your application.
+        </p>
+     </div>
+      <div className="bg-assumDevsPurpleLight rounded-md p-4">
+        <div className="flex items-center justify-between">
+            <h2 className="font-medium"> Student Orientation </h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-2 py-2">2026-12-5</span>
+        </div>
+        <p className="text-sm text-gray-500 mt-2">
+          Join us for an orientation session to get familiar with the campus and your courses.
+        </p>
+     </div>
+      <div className="bg-assumDevsYellowLight rounded-md p-4">
+        <div className="flex items-center justify-between">
+            <h2 className="font-medium"> Beginning of Semester </h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-2 py-2">2026-12-5</span>
+        </div>
+        <p className="text-sm text-gray-500 mt-2">
+          Welcome to the new semester! Please review the course syllabi and ensure all required documents are submitted.
+        </p>
+     </div>
+    </div>
+     </div>
+
+  );
+};
+
+export default Announcements;
--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -16,7 +16,7 @@ type Value = ValuePiece | [ValuePiece, ValuePiece]
 const events = [
   {
     id: 1,
-    title: "Earling morning coding",
+    title: "Morning coding",
     time: "04:00 AM - 06:05 AM",
     description: "Start your day with some coding to boost productivity and creativity. This is a great time to work on personal projects or learn new programming languages.",
   },
@@ -30,7 +30,7 @@ const events = [
 
   {
     id: 3,
-    title: "Breakfast and planning",
+    title: "Breakfast and Todos",
     time: "07:30 AM - 08:00 AM",
     description: "Enjoy a nutritious breakfast while planning your day. This is a good time to set your goals, prioritize tasks, and organize your schedule for the day ahead.",
   },
@@ -64,9 +64,9 @@ const EventCalendar = () => {
             key={event.id}>
               <div className="flex items-center justify-between">
                 <h1 className="font-semibold text-gray-600">{event.title}</h1>
-                 <span>{event.time}</span>
+                 <span className="text-xs text-gray-500">{event.time}</span>
               </div>
-              <p>{event.description}</p>
+              <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
             </div>
           ))}
         </div>
