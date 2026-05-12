5296cc4dc9ff073c71f3be36bf599d55dc298e35
Assumpta <assumptachinonso42@gmail.com>
2026-05-12 05:43:04 +0100

    new commit

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -55,12 +55,13 @@ const EventCalendar = () => {
     <div className='bg-white p-4 rounded-md'>
         <Calendar onChange={onChange} value={value} />
         <div className="flex items-center justify-between">
-          <h1>Events</h1>
+          <h1 className="text-xl font-semibold my-4">Events</h1>
           <Image src="/moreDark.png" alt="add event" width={20} height={20} />
         </div>
         <div className="flex flex-col gap-4">
           {events.map(event => (
-            <div className="" key={event.id}>
+            <div className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-assumDevs even:border-t-assumDevsPurple" 
+            key={event.id}>
               <div className="flex items-center justify-between">
                 <h1>{event.title}</h1>
                  <span>{event.time}</span>
