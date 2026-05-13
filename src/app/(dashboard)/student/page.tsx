a7b9da997737ec272b279c7bbce21c8d683c1286
Assumpta <assumptachinonso42@gmail.com>
2026-05-13 05:31:25 +0100

    new commit

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,11 @@
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      {/*LEFT*/}
+      <div className=""></div>
+        {/*RIGHt*/}
+      <div className=""></div>
+    </div>
   );
 };
 
