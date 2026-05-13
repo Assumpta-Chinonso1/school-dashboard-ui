db90c456c5f6da42ac67fabb16488c680321de3d
Assumpta <assumptachinonso42@gmail.com>
2026-05-13 05:40:05 +0100

    new commit

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -5,7 +5,9 @@ const StudentPage = () => {
   return (
     <div className='p-4 flex gap-4 flex-col xl:flex-row'>
       {/*LEFT*/}
-      <div className="w-full xl:w-2/3"></div>
+      <div className="w-full xl:w-2/3">
+        <div className=""></div>
+      </div>
        {/*RIGHT*/}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
       <EventCalendar/>
