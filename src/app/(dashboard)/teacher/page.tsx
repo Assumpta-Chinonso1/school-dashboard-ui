c45af7f4588fde301827bcfd413628594726cce8
Assumpta <assumptachinonso42@gmail.com>
2026-05-15 05:26:19 +0100

    new commit

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -4,11 +4,11 @@ import BigCalendar from "@/components/BigCalendar";
 
 const TeacherPage = () => {
   return (
-    <div className='flex-1 p-4 flex gap-4 flex-col xl:flex-row'>
+    <div className=' p-4 flex gap-4 flex-col xl:flex-row'>
       {/*LEFT*/}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule</h1>
+          <h1 className="text-xl font-semibold mb-4">Schedule</h1>
           <BigCalendar/>
         </div>
       </div>
