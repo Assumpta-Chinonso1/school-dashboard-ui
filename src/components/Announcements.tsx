8f25600894a11f950e0d4cc437094c02450b752a
Assumpta <assumptachinonso42@gmail.com>
2026-05-15 05:51:57 +0100

    new commit

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,6 +1,6 @@
 const Announcements = () => {
   return (
-    <div className='bg-white p-4 rounded-md'>
+    <div className='bg-white p-6 rounded-md'>
      <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold">Announcements</h1>
         <span className="text-xs font-gray-400">View all</span>
