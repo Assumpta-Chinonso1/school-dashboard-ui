331e63cd3fe174a60849b62a2595886f78bda48f
Assumpta <assumptachinonso42@gmail.com>
2026-05-16 05:23:13 +0100

    new commit

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,3 +1,5 @@
+import TableSearchPage from '@/components/TableSearch';
+
 const TeacherListPage = () => {
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
@@ -5,7 +7,10 @@ const TeacherListPage = () => {
       <div className="">
         <h1 > All Teachers</h1>
         <div className="">
-          
+          <TableSearchPage />
+          <div className="">
+            
+          </div>
         </div>
       </div>
       {/*Top*/}
