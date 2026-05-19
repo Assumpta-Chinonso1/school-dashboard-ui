f0e0156c55c1521dbc84831fff081d97a57e4964
Assumpta <assumptachinonso42@gmail.com>
2026-05-19 05:00:58 +0100

    new commit

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -2,7 +2,7 @@ const Pagination = () => {
   return (
     <div className='p-4 flex items-center justify-between text-gray-500'>
       <button disabled className="py-2 px-4 rounded-md bg-slate-200 text-sm font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Prev</button>
-      <div className="">
+      <div className="flex items-center gap-2 text-sm">
         <button className="px-2 rounded-sm bg-assumDevs" >1</button>
         <button className="px-2 rounded-sm" >2</button>
         <button className="px-2 rounded-sm" >3</button>
