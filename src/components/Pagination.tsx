96635e8cbc130884a4db1351a189d6335548f6e4
Assumpta <assumptachinonso42@gmail.com>
2026-05-19 04:47:20 +0100

    new commit

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,7 +1,7 @@
 const Pagination = () => {
   return (
     <div className='p-4 flex items-center justify-between text-gray-500'>
-      <button>Prev</button>
+      <button disabled className="py-2 px-4 rounded-md bg-slate-200 text-sm font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Prev</button>
     </div>
   );
 };
