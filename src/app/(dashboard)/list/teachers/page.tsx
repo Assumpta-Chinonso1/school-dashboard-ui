884d6f95295d407a9b64782bacb8902aff53241e
Assumpta <assumptachinonso42@gmail.com>
2026-05-20 14:37:03 +0100

    new commit

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -103,9 +103,11 @@ const renderRow = (item:Teacher) => (
              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-assumDevsYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-assumDevsYellow">
-              <Image src="/plus.png" alt="" width={14} height={14} />
-            </button>
+               {role === "admin" && (
+                          <button className="w-8 h-8 flex items-center justify-center rounded-full bg-assumDevsYellow">
+                           <Image src="/plus.png" alt="" width={14} height={14} />
+                         </button>
+                        )}
           </div>
         </div>
       </div>
