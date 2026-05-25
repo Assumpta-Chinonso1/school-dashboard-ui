422bc8d1bf65028cca7ad770b1e00d9458c149ff
Assumpta <assumptachinonso42@gmail.com>
2026-05-25 15:38:00 +0100

    new commit

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -50,7 +50,7 @@ const columns = [
 
 const ParentListPage = () => {
 
-const renderRow = (item:Student) => (
+const renderRow = (item:Parent) => (
   <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-assumDevsPurpleLight">
     <td className="flex items-center gap-4 p-4">
       <Image src={item.photo} alt='' width={40} height={40} className='md:hidden xl:block w-10 h-10 rounded-full object-cover' />
