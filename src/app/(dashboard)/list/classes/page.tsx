969b7cdea27cbc9e5b08bf890fa0babe19e76a2f
Assumpta <assumptachinonso42@gmail.com>
2026-06-03 05:34:26 +0100

    new commit

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -30,6 +30,11 @@ const columns = [
     accessor: "grade",
      className: "hidden md:table-cell"
   },
+  {
+    header: "Supervisor", 
+    accessor: "supervisor",
+     className: "hidden md:table-cell"
+  },
     {
     header: "Actions", 
     accessor: "actions",
@@ -41,7 +46,7 @@ const columns = [
 
 const ClassesListPage = () => {
 
-const renderRow = (item:Subject) => (
+const renderRow = (item:Class) => (
   <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-assumDevsPurpleLight">
     <td className="flex items-center gap-4 p-4">
         {item.name} </td>
@@ -67,7 +72,7 @@ const renderRow = (item:Subject) => (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
       {/*Top*/}
       <div className="flex items-center justify-between">
-        <h1 className=" hidden md:block text-lg font-semibold" > All Subjects</h1>
+        <h1 className=" hidden md:block text-lg font-semibold" > All Classes</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearchPage />
           <div className="flex items-center gap-4 self-end">
