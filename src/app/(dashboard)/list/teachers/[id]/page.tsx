85c4424405e75b2fcab432accfb2fe009c1d5698
Assumpta <assumptachinonso42@gmail.com>
2026-06-11 05:16:09 +0100

    new commit

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -9,10 +9,10 @@ const SingleTeachersPage = () => {
         <div className="flex flex-col lg:flex-row gap-4">
 
         {/*USER INFO CARD*/}
-        <div className="bg-assumDevs py-6 px-4 rounded-md flex-1"></div>
+        <div className="bg-assumDevs py-6 px-4 rounded-md flex-1 flex  gap-4"></div>
         
         {/*SMALL CARD*/}
-        <div className=""></div>
+        <div className="flex-1"></div>
 
         </div>
         {/*BOTTOM*/}
