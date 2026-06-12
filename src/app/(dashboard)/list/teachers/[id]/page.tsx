c9ddc781fdfe06e29ba37ea8765b6a7db1baa093
Assumpta <assumptachinonso42@gmail.com>
2026-06-12 05:18:29 +0100

    new commit

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -20,7 +20,7 @@ const SingleTeachersPage = () => {
           <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
 
-            <div className="">
+            <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
               <Image src="/blood.png" alt="" width={14} height={14} />
               <span>A+</span>
             </div>
