3a4ae95656d7f9887b3785eb097764fc6904341f
Assumpta <assumptachinonso42@gmail.com>
2026-06-12 05:24:45 +0100

    new commit

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -28,11 +28,11 @@ const SingleTeachersPage = () => {
               <Image src="/date.png" alt="" width={14} height={14} />
               <span>June 2026</span>
             </div>
-               
+              <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
               <Image src="/mail.png" alt="" width={14} height={14} />
               <span>AssumDevs@gmail.com</span>
             </div>
-            <div className="">
+              
               <Image src="/phone.png" alt="" width={14} height={14} />
               <span>+234 5634 2566 443</span>
             </div>
