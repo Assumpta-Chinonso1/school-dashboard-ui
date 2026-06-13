a43eff973c7d461ddebe01783c97ff27be438f95
Assumpta <assumptachinonso42@gmail.com>
2026-06-13 05:12:02 +0100

    new commit

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -51,6 +51,16 @@ const SingleTeachersPage = () => {
               <span className="text-sm text-gray-400" >Attendance</span>
              </div>
           </div>
+
+           {/*CARD*/}
+          <div className="">
+             <Image  src="/SingleAttendance.png" alt="" width={24} height={24} className="w-6 h-6" />
+             <div className="">
+              <h1 className="text-xl font-semibold" >90%</h1>
+              <span className="text-sm text-gray-400" >Attendance</span>
+             </div>
+          </div>
+          
         </div>
 
         </div>
