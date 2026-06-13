4746fb005e0c5c9f9c7600d68850f6ea6bdfd0e6
Assumpta <assumptachinonso42@gmail.com>
2026-06-13 05:10:43 +0100

    new commit

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -45,7 +45,11 @@ const SingleTeachersPage = () => {
         <div className="flex-1 flex gap-4 justify-between flex-wrap">
           {/*CARD*/}
           <div className="">
-             <Image  src="/SingleAttandance" alt="" width={} height={} />
+             <Image  src="/SingleAttendance.png" alt="" width={24} height={24} className="w-6 h-6" />
+             <div className="">
+              <h1 className="text-xl font-semibold" >90%</h1>
+              <span className="text-sm text-gray-400" >Attendance</span>
+             </div>
           </div>
         </div>
 
