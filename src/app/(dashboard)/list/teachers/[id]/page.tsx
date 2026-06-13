d188674e720f8a3a1ae5b483900df4d3ee698f0a
Assumpta <assumptachinonso42@gmail.com>
2026-06-13 05:20:39 +0100

    new commit

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -54,7 +54,7 @@ const SingleTeachersPage = () => {
 
            {/*CARD*/}
           <div className="">
-             <Image  src="/SingleAttendance.png" alt="" width={24} height={24} className="w-6 h-6" />
+             <Image  src="/SingleBranch.png" alt="" width={24} height={24} className="w-6 h-6" />
              <div className="">
               <h1 className="text-xl font-semibold" >2</h1>
               <span className="text-sm text-gray-400" >Branches</span>
@@ -63,19 +63,19 @@ const SingleTeachersPage = () => {
 
            {/*CARD*/}
           <div className="">
-             <Image  src="/SingleAttendance.png" alt="" width={24} height={24} className="w-6 h-6" />
+             <Image  src="/SingleLesson.png" alt="" width={24} height={24} className="w-6 h-6" />
              <div className="">
-              <h1 className="text-xl font-semibold" >90%</h1>
-              <span className="text-sm text-gray-400" >Attendance</span>
+              <h1 className="text-xl font-semibold" >6</h1>
+              <span className="text-sm text-gray-400" >Lessons</span>
              </div>
           </div>
 
            {/*CARD*/}
           <div className="">
-             <Image  src="/SingleAttendance.png" alt="" width={24} height={24} className="w-6 h-6" />
+             <Image  src="/SingleClass.png" alt="" width={24} height={24} className="w-6 h-6" />
              <div className="">
-              <h1 className="text-xl font-semibold" >90%</h1>
-              <span className="text-sm text-gray-400" >Attendance</span>
+              <h1 className="text-xl font-semibold" >6</h1>
+              <span className="text-sm text-gray-400" >Classes</span>
              </div>
           </div>
 
