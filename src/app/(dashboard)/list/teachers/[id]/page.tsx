107665a3c7530326e8f6e5de3ab961d497800f0e
Assumpta <assumptachinonso42@gmail.com>
2026-06-30 15:21:12 +0100

    30/6/2026

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -94,7 +94,7 @@ const SingleTeachersPage = () => {
 
 
         {/*RIGHT*/}
-        <div className="w-full xl:w-1/3">
+        <div className="w-full xl:w-1/3 flex flex-col gap-4">
         <div className="bg-white p-4 rounded-md">
               <h1 className="text-xl font-semibold">Shortcut</h1>
               <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
