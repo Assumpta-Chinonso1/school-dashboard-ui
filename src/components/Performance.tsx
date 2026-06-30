2a80512fe90d2aaeed00165827f06c48bd16fce9
Assumpta <assumptachinonso42@gmail.com>
2026-06-30 15:37:51 +0100

    new commit

--- src/components/Performance.tsx
+++ src/components/Performance.tsx
@@ -7,10 +7,7 @@ import { Pie, PieChart, Sector, Cell, ResponsiveContainer } from 'recharts';
 const data = [
   { name: 'Group A', value: 400 },
   { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 },
-  { name: 'Group F', value: 189 },
+
 ];
 
 const Performance = () => {
